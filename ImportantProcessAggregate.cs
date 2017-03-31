7877baad439e943a3a16316809d04575f147240b
jstarman <jstarman@incomm.com>
2017-03-30 22:06:10 -0700

    just getting started.

---
+++ ImportantProcessAggregate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventSourceSample
+{
+    public class ImportantProcessAggregate
+    {
+        public ImportantProcessAggregate(IEnumerable<ImportantEvent> events)
+        {
+
+        }
+
+        public void CreateAnImportantThing(string name, int age)
+        { }
+
+        public void AnotherImportantThingHappenedToTheImportantThing(DateTime when, string how) { }
+    }
+}
---
+++ Migrations/EventSample.sql
@@ -0,0 +1,13 @@
+﻿IF OBJECT_ID('[dbo].[EventSample]', 'U') IS NULL
+BEGIN
+CREATE TABLE EventSample(
+  Id uniqueidentifier not null,
+  EventType nvarchar(200) not null,
+  EventId int not null,
+  EventData nvarchar(MAX) not null,
+  MetaData nvarchar(MAX) null,
+  [CreatedOn] [datetime2](7) NOT NULL DEFAULT GETUTCDATE(),
+  Constraint PKEvents PRIMARY KEY(ID)
+)
+END
+GO
